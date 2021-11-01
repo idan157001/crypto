550a4eb25725d3e46461f80274e6db31c1158bba
idan157001 <61421500+idan157001@users.noreply.github.com>
2021-11-01 03:49:45 -0700

    Add files via upload

---
+++ crypto/web/app/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CsrfProtect
+
+from datetime import timedelta
+
+from flask_wtf.csrf import CSRFProtect
+
+app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.secret_key = b'dwa156dwa1d56a1fa61@'
+app.permanent_session_lifetime = timedelta(minutes=3)
+db = SQLAlchemy(app)
+
+
+from app import routes,models
